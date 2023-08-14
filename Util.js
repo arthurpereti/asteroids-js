e53d5ed5e7d7ab31a62404f6d20e8780cc534386
Arthur Pereti <arthurcoelho2005@outlook.com>
2023-08-14 08:03:18 -0300

    first commit

---
+++ Util.js
@@ -0,0 +1,115 @@
+class Obj {
+    constructor(x,y,w,h,at){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.at = at
+    }
+
+    des_obj(){
+        let img = new Image()
+        img.src = this.at
+        des.drawImage(img,this.x,this.y,this.w,this.h)
+    }
+}
+
+class Nave extends Obj{
+    dir = 0
+    pts = 0
+    vida = 5
+
+    mov(){
+        this.x += this.dir
+        if(this.x <= 0){
+            this.x =0
+        }else if(this.x >=450){
+            this.x = 450
+        }
+    }
+
+    point(objeto){
+        if(this.y+this.h < objeto.y){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    colid(objeto){
+        if((this.x < objeto.x+objeto.w)&&
+        (this.x+this.w > objeto.x)&&
+        (this.y < objeto.y + objeto.h)&&
+        (this.y + this.h > objeto.y)){
+            return true
+        }else{
+            return false
+        }
+    }
+}
+
+class Disco extends Obj{
+
+    mov(){
+        this.y += 7
+        if(this.y >= 700){
+            this.y = -200
+            this.x = (Math.random() * (448 - 2 +1)+2) // o disco tem 50 px de largura
+        }
+    }
+
+    recomeca(){
+        this.y = -200
+        this.x = (Math.random() * (448 - 2 +1)+2) // o disco tem 50 px de largura 
+    }
+
+    // acrescentado, verificar
+    colid(objeto) {
+        // Verifica colisão entre duas caixas delimitadoras retangulares
+        return (
+            this.x < objeto.x + objeto.w &&
+            this.x + this.w > objeto.x &&
+            this.y < objeto.y + objeto.h &&
+            this.y + this.h > objeto.y
+        )
+    }
+}
+
+class Tiro extends Obj{
+    des_tiro(){
+        des.fillStyle = this.at
+        des.fillRect(this.x, this.y, this.w, this.h)
+    }
+
+    mov(){
+        this.y -= 10
+    }
+    // acrescentado, verificar
+    colid(objeto) {
+        // Verifica colisão entre duas caixas delimitadoras retangulares
+        return (
+            this.x < objeto.x + objeto.w &&
+            this.x + this.w > objeto.x &&
+            this.y < objeto.y + objeto.h &&
+            this.y + this.h > objeto.y
+        )
+    }
+}
+
+class BG extends Obj{
+
+    mov(ini,lim){
+        this.y +=2
+        if(this.y > lim){
+            this.y = ini
+        }
+    }
+}
+
+class Texto{
+    des_text(texto,x,y,cor,font){
+        des.font = font
+        des.fillStyle = cor
+        des.fillText(texto,x,y)
+    }
+}
---
+++ space.js
@@ -0,0 +1,143 @@
+let des = document.getElementById('des').getContext('2d')
+
+let bg1 = new BG(0,0,500,700,'assets/background.jpg')
+let bg2 = new BG(0,-700,500,700,'assets/background2.jpg')
+let bg3 = new BG(0,-1400,500,700,'assets/background.jpg')
+let bg4 = new BG(0,-2100,500,700,'assets/background2.jpg')
+let nav1 = new Nave(200,520,50,70,'assets/nave.png')
+let disco1 = new Disco(100,-200,50,50,'assets/disco.png')
+let disco2 = new Disco(400,-400,50,50,'assets/disco2.png')
+let disco3 = new Disco(350,-600,50,50,'assets/disco3.png')
+let txt_pts = new Texto()
+let pts = new Texto()
+let txt_vidas = new Texto()
+let n_vidas = new Texto()
+const som1 = new Audio('assets/nave_som.mp3')
+const som2 = new Audio('assets/batida.mp3')
+som1.volume = 1.0
+som1.loop = true
+som2.volume = 0.7
+
+const discos = [disco1, disco2, disco3] // acrescentado, verificar
+const grupoTiros = []
+
+document.addEventListener('keydown', (ev)=>{
+    if(ev.key === 'a'){
+        nav1.dir -=5
+    }
+    if(ev.key === 'd'){
+        nav1.dir +=5
+    }
+    
+    
+})
+document.addEventListener('keyup', (ev)=>{
+    if(ev.key === 'a'){
+        nav1.dir = 0
+    }
+    if(ev.key === 'd'){
+        nav1.dir = 0
+    }
+})
+
+document.addEventListener('keypress', (ev)=>{
+    if (ev.key === 'l') {
+        let tiro = new Tiro(nav1.x - 4 + nav1.w / 2, nav1.y, 8, 16, 'red')
+        grupoTiros.push(tiro)
+    }
+    som1.play()
+})
+
+function pontos(){
+    if(nav1.point(disco1)){
+        nav1.pts +=1
+    }else if(nav1.point(disco2)){
+        nav1.pts +=1
+    }else if(nav1.point(disco3)){
+        nav1.pts +=1
+    }
+}
+
+function colisao(){
+    if(nav1.colid(disco1)){
+        disco1.recomeca()
+        nav1.vida -=1
+        som1.pause()
+        som2.play()
+    }else if(nav1.colid(disco2)){
+        disco2.recomeca()
+        nav1.vida -=1
+        som1.pause()
+        som2.play()
+    }else if(nav1.colid(disco3)){
+        disco3.recomeca()
+        nav1.vida -=1
+        som1.pause()
+        som2.play()
+    }
+}
+
+// acrescentado - verificar
+function tiros_nave() {
+    for (let i = 0; i < grupoTiros.length; i++) {
+        for (let j = 0; j < discos.length; j++) {
+            if (grupoTiros[i].colid(discos[j])) {
+                // Se houver colisão, remove o tiro e o disco do jogo
+                grupoTiros.splice(i, 1)
+                discos.splice(j, 1)
+
+                // Incrementa os pontos do jogador
+                nav1.pts += 1
+            }
+        }
+    }
+}
+
+function desenha(){    
+    bg1.des_obj()
+    bg2.des_obj()
+    bg3.des_obj()
+    bg4.des_obj()
+    disco1.des_obj()
+    disco2.des_obj()
+    disco3.des_obj()
+    nav1.des_obj()
+    grupoTiros.forEach((tiro)=>{
+        tiro.des_tiro()
+    })
+    txt_pts.des_text('Pontos:',20,40,'white','30px Times')
+    pts.des_text(nav1.pts,120,40,'white','30px Times')
+    txt_vidas.des_text('Vidas:',380,40,'white','30px Times')
+    n_vidas.des_text(nav1.vida,460,40,'white','30px Times')
+}
+
+function atualiza(){
+    bg1.mov(0,2100)
+    bg2.mov(-700,1400)
+    bg3.mov(-1400,700)
+    bg4.mov(-2100,0)
+    disco1.mov()
+    disco2.mov()
+    disco3.mov()
+    nav1.mov()
+    grupoTiros.forEach((tiro)=>{
+        tiro.mov()
+        if(tiro.y <= -50){
+            grupoTiros.splice(tiro[0],1)
+        }
+    })
+    tiros_nave() // acrescentado - verificar
+    colisao()
+    pontos()
+    
+}
+
+
+function main(){
+    des.clearRect(0,0,500,700)
+    desenha()
+    atualiza()
+    requestAnimationFrame(main)
+}
+
+main()
